3d4ba914d8193f637aa8a5ad59ec78527606d33a
corvaxx <maybe.mail@inbox.ru>
2020-11-06 19:06:43 +0300

    gop = 6
    Signed-off-by: corvaxx <maybe.mail@inbox.ru>

--- worker/src/RTC/RtpStream.cpp
+++ worker/src/RTC/RtpStream.cpp
@@ -463,8 +463,9 @@ namespace RTC
 			// av_opt_set(c.codecContext->priv_data, "tune",   "zerolatency", 0);
 			// c.codecContext->level = ;
 
-			c.codecContext->max_b_frames = 1;
-			c.codecContext->gop_size     = 1;
+			c.codecContext->max_b_frames =  0;
+			c.codecContext->gop_size     =  6;
+			c.codecContext->qcompress    = .8;
 
             int result = avcodec_open2(c.codecContext.get(), c.codec, nullptr);
             if (result < 0)
