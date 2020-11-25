7fe028fdd562e60b117b5672f0c1c621eef61af6
corvaxx <maybe.mail@inbox.ru>
2020-11-25 12:02:45 +0300

    bw
    Signed-off-by: corvaxx <maybe.mail@inbox.ru>

--- worker/include/RTC/Producer.hpp
+++ worker/include/RTC/Producer.hpp
@@ -139,7 +139,7 @@ namespace RTC
 		struct VideoOrientation videoOrientation;
 		struct TraceEventTypes traceEventTypes;
 
-		TranslateMode translateMode { direct };
+		TranslateMode translateMode { decodeAndEncode };
 
 		// 40 - 25 fps
 		// 67 - 15 fps
