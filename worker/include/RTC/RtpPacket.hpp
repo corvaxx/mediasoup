52b918515b74180bc398bbf21f7e6abfa730de27
corvaxx <maybe.mail@inbox.ru>
2020-12-21 00:48:03 +0300

    15fps for testing
    Signed-off-by: corvaxx <maybe.mail@inbox.ru>

--- worker/include/RTC/RtpPacket.hpp
+++ worker/include/RTC/RtpPacket.hpp
@@ -32,7 +32,7 @@ using json = nlohmann::json;
 
 #define RTP_FIXED_HEADER                12
 
-#define MIXER_FPS                       25
+#define MIXER_FPS                       15
 
 namespace RTC
 {
